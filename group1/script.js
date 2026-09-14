90f37a990be3b02a090973a3526151e0bba74e20
petrosbalyan-pixel <petros.balyan@gmail.com>
2026-09-15 00:05:53 +0400

    Add files via upload

---
+++ group1/script.js
@@ -0,0 +1,4 @@
+// Խումբ 1․ այստեղ գրեք կայքի JavaScript հրահանգները։
+document.querySelector('#hello-button').addEventListener('click', () => {
+  document.querySelector('#message').textContent = 'Կոճակը աշխատեց 🎉';
+});
